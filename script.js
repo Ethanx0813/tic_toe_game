1a9037b65c8100ebeec60a6e0482306b090331f7
Ethanx0813 <ankityadav770587@gmail.com>
2023-10-09 10:36:43 +0530

    my code commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let currentPlayer = "X";
+let arr = Array(9).fill(null);
+
+function checkWinner() {
+    if (
+        // Rows
+        (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2]) ||
+        (arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5]) ||
+        (arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8]) ||
+        // Columns
+        (arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6]) ||
+        (arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7]) ||
+        (arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8]) ||
+        // Diagonals
+        (arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8]) ||
+        (arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6])
+    ) {
+        // A player has won
+        document.write(`Winner is ${currentPlayer}`);
+    } else if (!arr.includes(null)) {
+        // It's a draw
+        document.write("It's a draw!");
+    }
+}
+
+function handleClick(el) {
+    const id = el.id;
+    if (arr[id] === null) {
+        arr[id] = currentPlayer;
+        el.innerText = currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        checkWinner(); // Check for a winner after each move
+    }
+    console.log(arr);
+}
